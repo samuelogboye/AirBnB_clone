fdfe6ef8b4d7782babb4234c2591805a8ee080f8
Samuel Ogboye <ogboyesam@gmail.com>
2023-08-09 17:16:38 +0100

    uniitest for model well implemented

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -2,21 +2,22 @@
 
 import unittest
 from datetime import datetime
+from time import sleep
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
 
+    def test_no_args_instantiates(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+        
     def test_init_with_kwargs(self):
-        data = {
-            "id": "123",
-            "created_at": "2023-08-01T12:00:00",
-            "updated_at": "2023-08-01T13:00:00"
-        }
-        instance = BaseModel(**data)
-        self.assertEqual(instance.id, "123")
-        self.assertEqual(instance.created_at, datetime(2023, 8, 1, 12, 0, 0))
-        self.assertEqual(instance.updated_at, datetime(2023, 8, 1, 13, 0, 0))
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        bm = BaseModel("12", id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(bm.id, "345")
+        self.assertEqual(bm.created_at, dt)
+        self.assertEqual(bm.updated_at, dt)
 
     def test_init_without_kwargs(self):
         instance = BaseModel()
@@ -24,6 +25,13 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(instance.created_at, datetime)
         self.assertIsInstance(instance.updated_at, datetime)
 
+    def test_instantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            BaseModel(id=None, created_at=None, updated_at=None)
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+        
     def test_str_representation(self):
         instance = BaseModel()
         str_repr = str(instance)
@@ -36,6 +44,12 @@ class TestBaseModel(unittest.TestCase):
         instance.save()
         self.assertNotEqual(original_updated_at, instance.updated_at)
 
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+
     def test_to_dict_method(self):
         instance = BaseModel()
         instance_dict = instance.to_dict()
@@ -44,25 +58,23 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn("id", instance_dict)
         self.assertIn("created_at", instance_dict)
         self.assertIn("updated_at", instance_dict)
-
-    def test_to_dict_created_at_format(self):
-        instance = BaseModel()
-        instance_dict = instance.to_dict()
-        self.assertTrue("created_at" in instance_dict)
-        self.assertIsInstance(instance_dict["created_at"], str)
-        datetime.strptime(instance_dict["created_at"], "%Y-%m-%dT%H:%M:%S")
-
-    def test_to_dict_updated_at_format(self):
-        instance = BaseModel()
-        instance_dict = instance.to_dict()
-        self.assertTrue("updated_at" in instance_dict)
-        self.assertIsInstance(instance_dict["updated_at"], str)
-        datetime.strptime(instance_dict["updated_at"], "%Y-%m-%dT%H:%M:%S")
-
+    
     def test_new_instance_unique_id(self):
         instance1 = BaseModel()
         instance2 = BaseModel()
         self.assertNotEqual(instance1.id, instance2.id)
+    
+    def test_two_instance_different_created_at(self):
+        instance1 = BaseModel()
+        sleep(0.05)
+        instance2 = BaseModel()
+        self.assertLess(instance1.created_at, instance2.created_at)    
+
+    def test_two_instance_different_updated_at(self):
+        instance1 = BaseModel()
+        sleep(0.05)
+        instance2 = BaseModel()
+        self.assertLess(instance1.updated_at, instance2.updated_at)    
 
     def test_init_with_invalid_created_at(self):
         with self.assertRaises(ValueError):
@@ -76,6 +88,9 @@ class TestBaseModel(unittest.TestCase):
             instance = BaseModel(**data)
             print(instance)
 
-
+    def test_args_unused(self):
+        bm = BaseModel(None)
+        self.assertNotIn(None, bm.__dict__.values())
+        
 if __name__ == '__main__':
     unittest.main()
