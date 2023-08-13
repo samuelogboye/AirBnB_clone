2a10a7270dcbc29725d834ca159eb454b713f713
Samuel Ogboye <125566090+samuelogboye@users.noreply.github.com>
2023-08-13 23:41:38 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,66 +2,120 @@ import unittest
 from unittest.mock import patch
 from io import StringIO
 from console import HBNBCommand
+import os
 
 
 class TestHBNBCommand(unittest.TestCase):
+    test_random_attributes = {
+        "strfoo": "barfoo",
+        "intfoo": 248,
+        "floatfoo": 9.8
+    }
+
+    attribute_values = {
+        str: "foobar108",
+        int: 1008,
+        float: 1.08
+    }
+
+    reset_values = {
+        str: "",
+        int: 0,
+        float: 0.0
+    }
+    
     def setUp(self):
+        """Sets up test cases."""
         self.console = HBNBCommand()
+        if os.path.isfile("file.json"):
+            os.remove("file.json")
+        self.resetStorage()
 
     def tearDown(self):
         pass
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_help_show(self, mock_stdout):
-        self.console.onecmd("help show")
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_help(self):
+        """Tests the help command."""
+        with patch('sys.stdout', new=StringIO()) as file:
+            self.console.onecmd("help")
+        expected_output = """
+Documented commands (type help <topic>):
+========================================
+EOF  all  count  create  destroy  help  quit  show  update
+
+"""
+        self.assertEqual(expected_output, file.getvalue())
+
+
+    
+    def test_help_show(self):
+        with patch('sys.stdout', new=StringIO()) as file:
+            self.console.onecmd("help show")
         expected_output = """string representation of object\n"""
-        self.assertIn(mock_stdout.getvalue(), expected_output)
+        self.assertEqual(expected_output, file.getvalue())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_create_with_valid_class(self, mock_stdout):
-        """Assert that output contains an ID"""
-        self.console.onecmd("create BaseModel")
-        self.assertIn("", mock_stdout.getvalue())
+    
+    def test_create_with_valid_class(self):
+         """Assert that output contains an ID"""
+        with patch('sys.stdout', new=StringIO()) as file:
+            self.console.onecmd("create BaseModel")
+        expected_output = ""
+        self.assertEqual(expected_output, file.getvalue())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_create_with_missing_class_name(self, mock_stdout):
-        self.console.onecmd("create")
+
+   
+    def test_create_with_missing_class_name(self):
+        with patch('sys.stdout', new=StringIO()) as file:
+            self.console.onecmd("create")
         expected_output = "** class name missing **\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+        self.assertEqual(expected_output, file.getvalue())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_show_with_valid_class_and_id(self, mock_stdout):
+    
+    def test_show_with_valid_class_and_id(self):
         """Assert that output contains the object"""
-        self.console.onecmd("create BaseModel")
-        obj_id = mock_stdout.getvalue().strip()
-        self.console.onecmd(f"show BaseModel {obj_id}")
-        self.assertIn(obj_id, mock_stdout.getvalue())
+        with patch('sys.stdout', new=StringIO()) as file:
+            self.console.onecmd("create BaseModel")
+            obj_id = file.getvalue().strip()
+            self.console.onecmd(f"show BaseModel {obj_id}")
+            self.assertEqual(obj_id, file.getvalue())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_show_with_missing_class_name(self, mock_stdout):
-        self.console.onecmd("show")
+   
+    def test_show_with_missing_class_name(self):
+        with patch('sys.stdout', new=StringIO()) as file:
+            self.console.onecmd("show")
         expected_output = "** class name missing **\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+        self.assertEqual(expected_output, file.getvalue())
 
-    @patch('sys.stdout', new_callable=StringIO)
+   
     def test_all_with_valid_class_name(self, mock_stdout):
         """Assert that output contains the object"""
-        self.console.onecmd("create BaseModel")
-        obj_id = mock_stdout.getvalue().strip()
-        self.console.onecmd(f"all BaseModel")
-        self.assertIn(obj_id, mock_stdout.getvalue())
+         with patch('sys.stdout', new=StringIO()) as file:
+            self.console.onecmd("create BaseModel")
+            obj_id = file.getvalue().strip()
+            self.console.onecmd(f"all BaseModel")
+            self.assertEqual(obj_id, file.getvalue())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_all_with_invalid_class_name(self, mock_stdout):
-        self.console.onecmd("all InvalidClass")
+    def test_all_with_invalid_class_name(self):
+        with patch('sys.stdout', new=StringIO()) as file:
+            self.console.onecmd("all InvalidClass")
         expected_output = "** class doesn't exist **\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+        self.assertEqual(expected_output, file.getvalue())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_update_with_invalid_class_name(self, mock_stdout):
-        self.console.onecmd('update InvalidClass 123 name "New Name"')
+
+    def test_update_with_invalid_class_name(self):
+         with patch('sys.stdout', new=StringIO()) as file:
+            self.console.onecmd('update InvalidClass 123 name "New Name"')
         expected_output = "** class doesn't exist **\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+        self.assertEqual(expected_output, file.getvalue())
+
+    
+
 
 
 if __name__ == '__main__':
